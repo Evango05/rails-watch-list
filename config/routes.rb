a25a3b2edf115d48726bda2140f637fe2403f68c
Evan Fabiani <fabiani.evan@icloud.com>
2023-02-24 11:40:52 +0100

    pas_grand_chose

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :new, :show, :create] do
-    resources :bookmarks, only: [:new, :create]
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
 end
